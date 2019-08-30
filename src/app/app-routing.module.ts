62b3d1b8a4ceb3ba227545931bdc2dabdc0b7e9e
Avery Duhalt <duhalt.avery@gmail.com>
2019-08-30 11:09:45 -0500

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipesStartComponent } from './recipes/recipes-start/recipes-start.component';
+import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+
+const appRoutes: Routes =[
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+    { path: 'recipes', component: RecipesComponent, children:[
+      { path: '', component: RecipesStartComponent },
+      { path: 'new', component: RecipeEditComponent },
+      { path: ':id', component: RecipesDetailComponent },
+      { path: ':id/edit', component: RecipeEditComponent },
+    ] },
+    { path: 'shopping-list', component: ShoppingListComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule{
+
+}
