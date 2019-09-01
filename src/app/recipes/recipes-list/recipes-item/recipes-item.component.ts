eed4d417c8c2a855f22e5b2ec2c8931303061123
Avery Duhalt <duhalt.avery@gmail.com>
2019-08-31 22:42:26 -0500

    Add files via upload

---
+++ src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../../recipe.module';
+
+@Component({
+  selector: 'app-recipes-item',
+  templateUrl: './recipes-item.component.html',
+  styleUrls: ['./recipes-item.component.css']
+})
+export class RecipesItemComponent implements OnInit {
+  @Input() recipe: Recipe;
+  @Input() index: number;
+
+
+  ngOnInit() {
+    // console.log(this.recipe);
+  }
+
+
+}
